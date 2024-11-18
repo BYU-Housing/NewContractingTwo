1312c531057e6c3a7908f3ae0c69950da469d7ed
Brandon <jacksonfamily20154@outlook.com>
2024-11-18 11:24:19 -0700

    Get Repo

---
+++ ReslifeFiveBackEnd/Repository/IGenRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReslifeFiveBackEnd.Repository
+{
+    public interface IGenRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Remove<T>(T entity) where T : class;
+        void Update<T>(T entity) where T : class;
+        void SaveChanges();
+        IQueryable<T> Set<T>() where T : class;
+        public Task AddAsync<T>(T entity) where T : class;
+    }
+}
