179c704397d6d867ec0a42dade067e05d15af4a2
Brandon <jacksonfamily20154@outlook.com>
2024-11-18 11:52:10 -0700

    Add IGenService

---
+++ ReslifeFiveBusinessLayer/Service/IGenService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReslifeFiveBusinessLayer.Service
+{
+    public interface IGenService
+    {
+        public IQueryable<T> GetModal<T>() where T : class;
+        public Task<T> GetByIdAsync<T>(int id) where T : class;
+        void Upsert<T>(T entity) where T : class;
+        void Add<T>(T entity) where T : class;
+        void Remove<T>(T entity) where T : class;
+        void Update<T>(T entity) where T : class;
+        void SaveChanges();
+    }
+}
