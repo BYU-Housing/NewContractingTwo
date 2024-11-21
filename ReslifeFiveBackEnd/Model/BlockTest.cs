6bf5abc232bf7d5fbbf1cd84b943ed814862d498
Brandon <jacksonfamily20154@outlook.com>
2024-11-21 12:04:44 -0700

    End Date is working

--- ReslifeFiveBackEnd/Model/BlockTest.cs
+++ ReslifeFiveBackEnd/Model/BlockTest.cs
@@ -11,5 +11,6 @@ namespace ReslifeFiveBackEnd.Model
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
     }
 }
