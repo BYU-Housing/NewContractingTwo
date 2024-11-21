ebb3159cb5fc9a34e64ec169da5aac8bf13e326b
Brandon <jacksonfamily20154@outlook.com>
2024-11-21 12:20:39 -0700

    Go the Room Number working

--- ReslifeFiveBackEnd/Model/BlockTest.cs
+++ ReslifeFiveBackEnd/Model/BlockTest.cs
@@ -12,5 +12,6 @@ namespace ReslifeFiveBackEnd.Model
         public string Name { get; set; } = string.Empty;
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+        public int? RoomNumber { get; set; }
     }
 }
