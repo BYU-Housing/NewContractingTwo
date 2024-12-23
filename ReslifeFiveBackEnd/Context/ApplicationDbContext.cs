6c48a41415bf518b274e60f73825ff92b916a747
Johnathan Fietkau <ijohnadon@gmail.com>
2024-12-23 13:21:13 -0600

    broken? not sure. gotta restart visual studio

--- ReslifeFiveBackEnd/Context/ApplicationDbContext.cs
+++ ReslifeFiveBackEnd/Context/ApplicationDbContext.cs
@@ -21,6 +21,7 @@ namespace ReslifeFiveBackEnd.Context
         public DbSet<TimePeriodTest> TimePeriodTest { get; set; }
         public DbSet<Role> Role { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<RLFImage> Images { get; set; }
 
     }
 }
---
+++ ReslifeFiveBackEnd/Model/RLFImage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReslifeFiveBackEnd.Model
+{
+    public class RLFImage
+    {
+        public int Id { get; set; }
+        public byte[]? ImageData { get; set; }
+        public string ImageType { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
