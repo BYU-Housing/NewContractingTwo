730394e758d2da49e292cf46cea4b8c1b109c519
Brandon Jackson <brandon_jackson@byu.edu>
2024-12-22 17:13:05 -0700

    AllocationTest

---
+++ ReslifeFiveBackEnd/Model/AllocationTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReslifeFiveBackEnd.Model
+{
+    public class AllocationTest
+    {
+        public int Id { get; set; }
+        public int? Area { get; set; }
+        public int? Building { get; set; }
+        public int? Floor { get; set; }
+        public string Bedspace { get; set; } = string.Empty;
+
+    }
+}
