c93f9f6bf52458f7ed889692abf20d43ad9d3daf
Johnathan Fietkau <ijohnadon@gmail.com>
2024-12-30 13:48:22 -0600

    fixed top bar sizing. added explicit target and active user references. included more information in the autocomplete.

--- ReslifeFiveBackEnd/Model/User.cs
+++ ReslifeFiveBackEnd/Model/User.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
+
+
 
 namespace ReslifeFiveBackEnd.Model
 {
@@ -14,9 +18,6 @@ namespace ReslifeFiveBackEnd.Model
         public string Surname { get; set; } = string.Empty;
         public string RestOfName { get; set; } = string.Empty;
         public string PreferredFirstName { get; set; } = string.Empty;
-        
-        [NotMapped]
-        public string PreferredFullName => PreferredFirstName + " " + Surname;
         public int Gender { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string MaritalStatus { get; set; } = string.Empty;
@@ -26,6 +27,10 @@ namespace ReslifeFiveBackEnd.Model
         public string CitizenshipCountry { get; set; } = string.Empty;
         public int RoleId { get; set; }
         public virtual Role? Role { get; set; }
+        [NotMapped]
+        public string PreferredFullName => PreferredFirstName + " " + Surname;
+        [NotMapped]
+        public string SearchBarInformation => $"{PreferredFullName}\n{NetId} {ByuId}\n{DateOfBirth:MM-dd-yyyy}";
 
     }
 }
